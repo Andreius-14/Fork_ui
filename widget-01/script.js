b1a607abaf193adb447c001e70915f888338d4be
Andreius_14 <Github_404@outlook.com>
2024-07-01 22:20:10 -0500

    Url-Data

--- widget-01/script.js
+++ widget-01/script.js
@@ -1,49 +1,60 @@
-const allLinks = document.querySelectorAll(".tabs a");
-const allTabs = document.querySelectorAll(".tab-content");
+// All <a>
+const allLinks = document.querySelectorAll(".tabs a"); 
+// All <Articles>
+const allTabs = document.querySelectorAll(".tab-content"); 
+// Padre De <Articles>
 const tabContentWrapper = document.querySelector(".tab-content-wrapper");
 
+// Parametro Id Del <a>  [Desplaza Scroll Lista]
 const shiftTabs = (linkId) => {
+  // verifica  <Article> 1 , <Article>2 ... <Articles>Final
   allTabs.forEach((tab, i) => {
+
+    // <Article> Tiene Coincidencia con ID <a>
     if (tab.id.includes(linkId)) {
+
       allTabs.forEach((tabItem) => {
-        tabItem.style = `transform: translateY(-${i * 300}px);`;
+        tabItem.style = `transform: translateY(-${ i *300}px);`;
       });
     }
   });
 };
 
+// [DA Luz a la Lista Cliqueada]
+// [Envia Id <a> Cliqueado a La funcion Scrool]
 allLinks.forEach((elem) => {
-  elem.addEventListener("click", function () {
-    const linkId = elem.id;
-    const hrefLinkClick = elem.href;
 
-    allLinks.forEach((link, i) => {
-      if (link.href == hrefLinkClick) {
-        link.classList.add("active");
-      } else {
-        link.classList.remove("active");
-      }
+  // Triggers <a> [Click] [Del Elemento Cliqueado Optenemos los Datos]
+  elem.addEventListener("click", function () {
+
+    const linkId = elem.id;              
+    const hrefLinkClick = elem.href;  
+
+    // Cheamos All <a> [Verificamos Cual tiene el id cliqueado, Añade Class]
+    allLinks.forEach((link, i) => {   
+      link.classList.toggle('active', link.href === hrefLinkClick);
     });
 
     shiftTabs(linkId);
   });
 });
 
-//? handle proper selection for initial load
-const currentHash = window.location.hash;
+//    - Guarda Datos en URL -- window.location
+//    Habilitar por defecto a la Primera de la Lista
 
-let activeLink = document.querySelector(`.tabs a`);
+const currentHash = window.location.hash;  // Optiene Id de URL
+let activeLink = document.querySelector(`.tabs a`); // Selecciona Primer <A>
 
+// Usar Si hay # en la URL
 if (currentHash) {
-  const visibleHash = document.getElementById(
-    `${currentHash.replace("#", "")}`
-  );
+  // Optiene ID <a> Del URL
+  const visibleHash = document.getElementById( `${currentHash.replace("#", "")}`);
 
   if (visibleHash) {
-    activeLink = visibleHash;
+    activeLink = visibleHash; // Seleccion Predeterminado = el # del url
   }
 }
 
 activeLink.classList.toggle("active");
 
-shiftTabs(activeLink.id);
+shiftTabs(activeLink.id);  //Scrool Por Defecto
